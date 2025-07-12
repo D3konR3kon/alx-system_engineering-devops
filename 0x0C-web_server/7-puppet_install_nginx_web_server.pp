30154f924e73ada7f6b0b453e41bbae5adfef9d3
Vuyo Dila <103577606+D3konR3kon@users.noreply.github.com>
2025-07-12 10:05:12 +0200

    Puppet manifest to install and configure Nginx web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,49 @@
+# Puppet manifest to install and configure Nginx web server
+# with Hello World page and 301 redirect
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => "Hello World!\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    # 301 Redirect for /redirect_me
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
